fce5dc8b8e41ae6cc490d9ef0f37ac9aa1339439
Andrei Dorian Duma <andrei@andreiduma.ro>
2022-09-17 18:50:09 +0200

    Add preview

--- app.py
+++ app.py
@@ -1,30 +1,57 @@
+import random
+import string
 import tempfile
 
 import cv2 as cv
 
-from flask import Flask, request, render_template
-from scripts.segmentation.test_one_image import interface
+from flask import Flask, request, render_template, redirect
+from scripts.segmentation.test_one_image import interface, image2image
 
 app = Flask(__name__)
 
 
-@app.route("/")
+def random_filename():
+    "".join(random.choices(string.ascii_uppercase + string.digits, k=6))
+
+
+@app.route("/detect", methods=["GET"])
 def index():
-    return render_template("index.html")
+    return render_template("detect.html", action="/detect")
 
 
 @app.route("/detect", methods=["POST"])
 def upload_file():
     image = request.files["image"]
-    with tempfile.TemporaryDirectory() as tmpdir:
-        path = tmpdir + "/" + image.filename
-        image.save(path)
 
-        img = cv.imread(path)
+    path = "uploads/" + random_filename()
+    image.save(path)
 
-        points = interface(img)
-        print(points)
+    img = cv.imread(path)
+    points = interface(img)
+    print(points)
 
-        return {
-            "points": points,
-        }
+    return {
+        "points": points,
+    }
+
+
+@app.route("/detect/preview", methods=["GET"])
+def index():
+    return render_template("detect.html", action="/detect/preview")
+
+
+@app.route("/detect/preview", methods=["POST"])
+def upload_file():
+    image = request.files["image"]
+
+    filename = random_filename()
+    path = "uploads/" + filename
+    image.save(path)
+
+    img = cv.imread(path)
+    img_out = image2image(img)
+
+    path_out = "static/" + filename
+    cv.imwrite(path_out, img_out)
+
+    return redirect(path_out)
